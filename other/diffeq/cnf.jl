8856135325663ad23392100d9c8ce96ca3013483
jessebett <jessebett@gmail.com>
2019-04-29 13:33:42 -0400

    Forward pass works, back has Float32 error

--- other/diffeq/cnf.jl
+++ other/diffeq/cnf.jl
@@ -22,7 +22,7 @@ prob = ODEProblem(cnf,nothing,tspan,nothing)
 params = Params([p])
 
 function predict_adjoint(x)
-    diffeq_adjoint(p,prob,Tsit5(),u0=[x;false],
+    diffeq_adjoint(p,prob,Tsit5(),u0=[x;zero(x)],
                    saveat=0.0:0.1:10.0,
                    sensealg=DiffEqFlux.SensitivityAlg(quad=false,
                                 backsolve=true,autojacvec=true))
@@ -30,9 +30,9 @@ end
 
 function loss_adjoint(xs)
     pz = Normal(0.0, 1.0)
-    preds = [predict_adjoint(x)[:,end] for x in xs]
-    z = [pred[1] for pred in preds] # TODO better slicing
-    delta_logp = [pred[2] for pred in preds]
+    preds = predict_adjoint(xs)[:,:,end]
+    z = preds[1:end-1,:]
+    delta_logp = preds[end:end,:]
 
     logpz = logpdf.(pz, z)
     logpx = logpz - delta_logp
@@ -44,11 +44,6 @@ opt = ADAM(0.1)
 const BATCH_SIZE = 100
 raw_data = Float32.(rand(Normal(2.0, 0.1),BATCH_SIZE))' # (D,BS)
 data = Iterators.repeated([raw_data], 1);
-
-diffeq_adjoint(p,prob,Tsit5(),u0=[raw_data;zero(raw_data)],
-                   saveat=0.0:0.1:10.0,
-                   sensealg=DiffEqFlux.SensitivityAlg(quad=false,
-                                backsolve=true,autojacvec=true))
 
 loss_adjoint(raw_data)
 
