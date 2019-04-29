b83720f87c44f76183151978868258298cea4285
jessebett <jessebett@gmail.com>
2019-04-29 13:01:59 -0400

    Chris's comment for CNF

---
+++ other/diffeq/cnf.jl
@@ -0,0 +1,51 @@
+using OrdinaryDiffEq
+using Distributions
+using Flux, DiffEqFlux, ForwardDiff, Tracker
+
+# Neural Network
+nn = Chain(Dense(1,1,tanh))
+p = DiffEqFlux.destructure(nn)
+tspan = Float32.((0.0, 10.0))
+function cnf(u,p,t)
+  z = @view u[1:end-1]
+  m = DiffEqFlux.restructure(nn,p)
+  jac = -sum(Tracker.jacobian((z)->log.(z), z))
+  if u isa TrackedArray
+      res = Tracker.collect([m(z);jac])
+  else
+      res = Tracker.data([m(z);jac])
+  end
+  res
+end
+
+prob = ODEProblem(cnf,nothing,tspan,nothing)
+params = Params([p])
+
+function predict_adjoint(x)
+    diffeq_adjoint(p,prob,Tsit5(),u0=[x;false],
+                   saveat=0.0:0.1:10.0,
+                   sensealg=DiffEqFlux.SensitivityAlg(quad=false,
+                                backsolve=true,autojacvec=true))
+end
+
+function loss_adjoint(xs)
+    pz = Normal(0.0, 1.0)
+    preds = [predict_adjoint(x)[:,end] for x in xs]
+    z = [pred[1] for pred in preds] # TODO better slicing
+    delta_logp = [pred[2] for pred in preds]
+
+    logpz = logpdf.(pz, z)
+    logpx = logpz - delta_logp
+    loss = -mean(logpx)
+end
+
+opt = ADAM(0.1)
+
+raw_data = [Float32[rand(Normal(2.0, 0.1)) for i in 1:100]]
+data = Iterators.repeated(raw_data, 1);
+
+loss_adjoint(raw_data[1])
+
+Flux.train!(loss_adjoint, params, data, opt)
+iszero(Tracker.grad(nn[1].W))
+
